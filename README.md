1144bdd070c65fd32016d47b2763030e00776bab
Alexey Kiselev <kiselev@bitrete.ru>
2012-02-10 17:32:13 +0400

    Added port configuration for RMI registry and remote repository object. Correct shutdown routine implemented. Short description written.

--- README.md
+++ README.md
@@ -3,8 +3,21 @@ Bitrete Jackrabbit RMI publishing servlet for Apache Tomcat
 
 Problem
 -------
-When you deploy Jackrabbit repository into Tomcat as [shared resource (deployment Model 2)](http://jackrabbit.apache.org/deployment-models.html#DeploymentModels-Model2%3ASharedJ2EEResource), and then ...
+When you deploy Jackrabbit repository into Tomcat as [shared resource (deployment Model 2)](http://jackrabbit.apache.org/deployment-models.html#DeploymentModels-Model2%3ASharedJ2EEResource), and then you try to enable RMI access to shared repository using RepositoryStartup servlet you get failed. The reason is that RepositoryStartup servlet can only publish locally created repository but not shared one. [See this for details](http://jackrabbit.510166.n4.nabble.com/Providing-RMI-access-for-a-model-2-shared-deployment-td512615.html).
 
-Coming soon.
+
+Solution
+--------
+To solve this problem you need to write simple servlet which gets repository reference via JNDI and publish it via RMI. So we did it for you :)
+
+How to build
+------------
+Just run provided ant script by following command line:
+ant /release distr
+It produces ready-to-deploy war file in folder distr/war.
+
+Configuration
+-------------
+You can configure RMI and repository ports in servlet's web.xml. All settings are well documented there. 
 
 Feel free to ask questions about this product info [at] bitrete.ru. ([www.bitrete.ru](http://www.bitrete.ru))
--- src/ru/bitrete/content/repository/publication/rmi/RmiPublicationServlet.java
+++ src/ru/bitrete/content/repository/publication/rmi/RmiPublicationServlet.java
@@ -17,7 +17,6 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
 import org.apache.jackrabbit.rmi.remote.RemoteRepository;
-import org.apache.jackrabbit.rmi.server.RemoteAdapterFactory;
 import org.apache.jackrabbit.rmi.server.ServerAdapterFactory;
 
 public class RmiPublicationServlet extends GenericServlet {
@@ -26,17 +25,19 @@ public class RmiPublicationServlet extends GenericServlet {
 	
 	private static final String REPOSITORY_JNDI_NAME_PARAMETER = "repository-jndi-name";
 	private static final String RMI_SERVICE_NAME_PARAMETER = "rmi-service-name";
-	private static final String RMI_PORT_PARAMETER = "rmi-port";
-	
+	private static final String RMI_REGISTRY_PORT_PARAMETER = "rmi-registry-port";
+	private static final String REPOSITORY_PORT_PARAMETER = "repository-port";
+
 	private static final String DEFAULT_JNDI_NAME = "jcr/repository";
 	private static final String DEFAULT_RMI_SERVICE_NAME = "JackrabbitRMI";
-	private static final int DEFAULT_RMI_PORT_NUMBER = 1099;
+	private static final int DEFAULT_RMI_REGISTRY_PORT_NUMBER = 1099;
+	private static final int DEFAULT_REPOSITORY_PORT_NUMBER = 1100;
 
 	private RemoteRepository remoteRepository;
 	private String rmiServiceName;
 
 	private Registry rmiRegistry;
-	
+
 	@Override
 	public void service(ServletRequest request, ServletResponse response)
 			throws ServletException, IOException {
@@ -48,31 +49,33 @@ public class RmiPublicationServlet extends GenericServlet {
 
 		rmiServiceName = getRmiServiceName(config);
 		String repositoryName = getRepositoryJndiName(config);
-		String portNumberValue = getRmiPortNumber(config);
+		int rmiPortNumber = getPortNumber(config, RMI_REGISTRY_PORT_PARAMETER, DEFAULT_RMI_REGISTRY_PORT_NUMBER);
+		int repositoryPortNumber = getPortNumber(config, REPOSITORY_PORT_PARAMETER, DEFAULT_REPOSITORY_PORT_NUMBER);
 		
 		log(String.format("Starting servlet on '%s' for repository '%s'", 
 				rmiServiceName, repositoryName));
 		
 		try {
-			int rmiPortNumber = Integer.parseInt(portNumberValue);
-
 			InitialContext context = new InitialContext();
 			Context environment = (Context) context.lookup("java:comp/env");
 
 			Repository repository = (Repository) environment
 					.lookup(repositoryName);
 
-			RemoteAdapterFactory factory = new ServerAdapterFactory();
+			ServerAdapterFactory factory = new ServerAdapterFactory();
+			factory.setPortNumber(repositoryPortNumber);
+			
 			remoteRepository = factory.getRemoteRepository(repository);
 			
 			log("Remote repository instance created");
 			
-			rmiRegistry = getOrCreateRegistry(rmiPortNumber);
+			rmiRegistry = createOrGetRegistry(rmiPortNumber);
+			if (rmiRegistry == null)
+				throw new IllegalStateException("Failed to get RMI registry, see log for details");
 			
 			log("RMI registry created");
 			
-			if (rmiRegistry != null)
-				rmiRegistry.rebind(rmiServiceName, remoteRepository);
+			rmiRegistry.rebind(rmiServiceName, remoteRepository);
 			
 			log(String.format("Remote repository registered on '%s'", rmiServiceName));
 		}
@@ -84,29 +87,12 @@ public class RmiPublicationServlet extends GenericServlet {
 		}
 	}
 
-	private Registry getOrCreateRegistry(int rmiPortNumber) {
-		Registry result = null;
-		try {
-			result = LocateRegistry.createRegistry(rmiPortNumber);
-		}
-		catch (RemoteException exception) {
-			log(String.format("Failed to create RMI registry on port %d", 
-					rmiPortNumber), exception);
-		}
-		
-		return result;
-	}
-
-	private String getRmiPortNumber(ServletConfig config) {
-		return getContextParameter(config, RMI_PORT_PARAMETER, 
-				Integer.toString(DEFAULT_RMI_PORT_NUMBER)); 
-	}
-
 	@Override
 	public void destroy() {
 		try {
 			if (rmiRegistry != null) {
 				rmiRegistry.unbind(rmiServiceName);
+				UnicastRemoteObject.unexportObject(remoteRepository, true);
 				UnicastRemoteObject.unexportObject(rmiRegistry, true);
 				log("RMI registry closed");
 			}
@@ -116,6 +102,32 @@ public class RmiPublicationServlet extends GenericServlet {
 		}
 		
 		super.destroy();
+	}
+
+	private Registry createOrGetRegistry(int rmiPortNumber) {
+		Registry result = null;
+		try {
+			result = LocateRegistry.createRegistry(rmiPortNumber);
+		}
+		catch (RemoteException exception) {
+			log(String.format("Failed to create RMI registry on port %d", 
+					rmiPortNumber), exception);
+			
+			try {
+				result = LocateRegistry.getRegistry(rmiPortNumber);
+			}
+			catch (RemoteException ex) {
+				log(String.format("Failed to get reference to remote RMI registry on port %d", 
+						rmiPortNumber));
+			}
+		}
+		
+		return result;
+	}
+
+	private int getPortNumber(ServletConfig config, String parameterName, int defaultValue) {
+		return Integer.parseInt(getContextParameter(config, parameterName, 
+				Integer.toString(defaultValue)));
 	}
 
 	private String getRepositoryJndiName(ServletConfig config) {
